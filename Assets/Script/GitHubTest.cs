60aaf208b787e43713cc8087db3f1fad7dff83d0
AL13aris <al13aris1133@gmail.com>
2024-09-30 12:46:46 +0900

    Main

---
+++ Assets/Script/GitHubTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitHubTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //test Main
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
