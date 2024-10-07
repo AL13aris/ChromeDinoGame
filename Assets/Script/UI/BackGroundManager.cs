5df36ec62ba20b315ed7cd8d07f9b78485d4e6a1
AL13aris <al13aris1133@gmail.com>
2024-10-07 12:56:36 +0900

    배경 스크롤링 추가

--- Assets/Script/UI/BackGroundManager.cs
+++ Assets/Script/UI/BackGroundManager.cs
@@ -1,24 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Sprites;
 using UnityEngine;
 
 public class BackGroundManager : MonoBehaviour
 {
+
+    public float speed;
+    float offset_x;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ScrollBackGround();
+
     }
 
     //배경을 일정속도로 움직임
-    private void ScrollBackGround()
+    public void ScrollBackGround()
     {
+        offset_x += (speed * Time.deltaTime);
+
+        Vector2 offset = new Vector2(offset_x, 0);
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", offset);
 
     }
 
