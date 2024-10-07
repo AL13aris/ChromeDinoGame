23aec745b74a33f3634bbfd6b73dcf43cb07c659
AL13aris <al13aris1133@gmail.com>
2024-10-07 10:48:21 +0900

    플레이어 함수 추가

--- Assets/Script/PlayerCtrl/PlayerCtrl.cs
+++ Assets/Script/PlayerCtrl/PlayerCtrl.cs
@@ -21,6 +21,12 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Jump();
+
+    }
+
+    private void Jump()// 화면을 누르면 플레이어가 점프를 합니다
+    {
         if (!bIsJumping)
         {
             if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -28,29 +34,54 @@ public class PlayerCtrl : MonoBehaviour
 
                 Debug.LogWarning("점프키를 눌렀습니다");
 
-                Jump();
+                Debug.LogWarning("JUMP");
+                bIsJumping = true;
+                rb.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
 
             }
         }
-       
-
-
-
     }
-
-    private void Jump()
-    {
-        Debug.LogWarning("JUMP");
-        bIsJumping = true;
-        rb.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("GROUND"))
         {
             Debug.LogWarning("GROUND");
-            bIsJumping = false ;
+            bIsJumping = false;
+            
         }
     }
+    private void Hit()
+    {
+        // 플레이어가 맞는 판정 <-얘는 OnCollisionEnter2D로 가는게 나을듯?
+    }
+
+    private void Attack()
+    {
+        //시간 남을때 구현 예정
+    }
+
+    private void UseItem()
+    {
+        //시간남을때 구현 예정
+    }
+
+    private void GetItem()
+    {
+        //시간남을때 구현 예정
+
+    }
+
+    private void ButtonAttack()
+    {
+        //시간남을때 구현 예정
+    }
+
+    //private void Gravity()
+    //{
+    //    //필요 없음 유니티 엔진 내부에서 자체 처리
+    //}
+
+
+
+
 }
